1b8c453b6199a8d6f46c6d8e805c7cfd20f02d69
Cole Rau <colevrau@gmail.com>
2021-01-29 21:29:29 -0800

    got like/dislike ratio working

--- src/components/Like.js
+++ src/components/Like.js
@@ -4,13 +4,24 @@ const Like = (props) => {
 
   const [like, setLike] = useState(0)
   const [dislike, setDislike] = useState(0)
+  const [ratio, setRatio] = useState(0)
+
+  const handleLike = () => {
+    setLike(like + 1)
+  }
+
+  const handleDislike = () => {
+    setDislike(dislike + 1)
+  }
 
   return (
     <div>
       <p>Likes: {like}</p>
       <p>Dislikes: {dislike}</p>
-      <button onClick={() => setLike(like + 1)}>Like</button>
-      <button onClick={() => setDislike(dislike + 1)}>Dislike</button>
+      <p>Like/Dislike Ratio: {like / dislike}</p>
+      <button onClick={handleLike}>Like</button>
+      <button onClick={handleDislike}>Dislike</button>
+
     </div>
   )
 }
