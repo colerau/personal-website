1d73a99c5ea0639e7e95507afc8fa3b7edbf6bec
Cole Rau <colevrau@gmail.com>
2021-02-06 04:31:13 -0800

    added spotify link

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Like from './components/Like.js'
 import CommentForm from './components/CommentForm.js'
 import About from './components/About.js'
+import NavBar from './components/NavBar.js'
 
 const App = () => {
   return (
     <div className="App">
       <h1>Cole Rau</h1>
       <About />
+      <br />
+      <NavBar />
       <CommentForm />
       <p>This is a sample comment</p>
       <Like />
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+function NavBar(props) {
+  return (
+    <>
+      <a href="https://open.spotify.com/artist/2Kpxy0Nc8cDbZF0gbahaQM" target="_blank" rel="noreferrer nofollow" alt="Spotify">Spotify</a>
+    </>
+  )
+}
+
+export default NavBar
