d1beae8eed0e8a777463385a6a65595998157c71
Cole Rau <colevrau@gmail.com>
2021-02-06 03:55:47 -0800

    made CommentForm with useState hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import Like from './components/Like.js'
-import Form from './components/Form.js'
+import CommentForm from './components/CommentForm.js'
 
 const App = () => {
-  const test = (num) => {
-    console.log(`like button clicked ${num} time(s)`)
-  }
+  // const test = (num) => {
+  //   console.log(`like button clicked ${num} time(s)`)
+  // }
   return (
     <div className="App">
 
-      <Like test={test}/>
-      <br />
-      <Form />
+      {/* <Like test={test}/>
+      <br /> */}
+      <CommentForm />
     </div>
   );
 }
---
+++ src/components/CommentForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+// class Form extends PureComponent {
+//   state = {
+//     name: ""
+//   }
+
+//   handleChange = (event) => {
+//     this.setState({
+//       name: event.target.value
+//     })
+//   }
+
+//   handleSubmit = (event) => {
+//     event.preventDefault()
+//     console.log("submit button clicked")
+//     this.setState({
+//       name: ""
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <form onSubmit={this.handleSubmit}>
+//           <input type="text" value={this.state.name} onChange={this.handleChange} />
+//           <input type="submit" value="Submit Name" />
+//         </form>
+//       </div>
+//     )
+//   }
+// }
+
+// export default Form
+
+function CommentForm(props) {
+
+  const [comment, setComment] = useState("")
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log(`submitting ${comment}`)
+  }
+
+  return (
+    <div>
+      <br />
+      <form onSubmit={handleSubmit}>
+        <label>
+          New Comment:
+          <input type="text" value={comment} onChange={e => setComment(e.target.value)}/>
+        </label>
+        <input type="Submit" />
+      </form>
+    </div>
+  )
+}
+
+export default CommentForm
